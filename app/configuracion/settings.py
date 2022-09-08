1e1cd8a05b4dda2eb3285200000814660b0e0d80
marco_romeromon <marco.romero@eon.com.mx>
2022-09-07 21:14:28 -0500

    Se abilitan todos los allowed host

--- app/configuracion/settings.py
+++ app/configuracion/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure--5pe&1h4(^fqslzorvgk9bf4x%3ilx+30gsadcu*ha)%28dgt#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
